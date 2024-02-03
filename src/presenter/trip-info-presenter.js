34f21867af948b3f3e22f294b27aa7a7eeebe420
Andrey Ozeev <andrey.ozeev@mail.ru>
2024-02-03 12:59:44 +0300

    start make trip-info

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -4,18 +4,28 @@ import HeaderTripInfoView from '../view/header-trip-info-view.js';
 export default class TripInfoPresenter {
   #container = null;
   #eventPointsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
   #eventPoints = null;
   #tripInfoComponent = null;
 
-  constructor({container, eventPointsModel}) {
+  constructor({container, eventPointsModel, destinationsModel, offersModel}) {
     this.#container = container;
     this.#eventPointsModel = eventPointsModel;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
     this.#eventPoints = this.#eventPointsModel.get();
+
+    this.#eventPointsModel.addObserver(this.#modelEventHandler);
   }
 
   init() {
     const prevTripInfoComponent = this.#tripInfoComponent;
-    this.#tripInfoComponent = new HeaderTripInfoView();
+    this.#tripInfoComponent = new HeaderTripInfoView({
+      destinations: this.#destinationsModel.get(),
+      offers: this.#offersModel.get(),
+      eventPoints: this.#eventPointsModel.get(),
+    });
     if (!prevTripInfoComponent) {
       render(this.#tripInfoComponent, this.#container, RenderPosition.AFTERBEGIN);
       return;
@@ -25,5 +35,9 @@ export default class TripInfoPresenter {
     remove(prevTripInfoComponent);
 
     render(this.#tripInfoComponent, this.#container);
+  }
+
+  #modelEventHandler = () => {
+    this.init()
   }
 }
